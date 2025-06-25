2380a1f0f651684e6ff9822c809909f3c865971c
jitabhsin <jitabhsin@gmail.com>
2025-06-25 10:34:05 +0530

    Week 2 Setting Up JUNIT

---
+++ WEEK 2/Setting Up JUnit/CODE/JUNIT_EXAMPLE/src/test/java/org/example/TestClass.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestClass {
+
+
+    @Test
+    public void test() {
+        int sum = 2 + 3;
+        System.out.println("Sum is: " + sum);
+
+    }
+
+}
