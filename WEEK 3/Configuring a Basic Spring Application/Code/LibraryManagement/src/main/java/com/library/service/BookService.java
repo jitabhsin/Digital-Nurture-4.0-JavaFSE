1d0263f452b4b735c790b5363b1fc2b7a4c0138c
ABHISHEK SINGH <146831587+jitabhsin@users.noreply.github.com>
2025-06-30 18:22:12 +0530

    Update BookService.java
    update setting up springboot

--- WEEK 3/Configuring a Basic Spring Application/Code/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ WEEK 3/Configuring a Basic Spring Application/Code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -6,7 +6,7 @@ public class BookService {
 
 	  private BookRepository bookRepository;
 
-	    // Setter Injection
+	    
 	    public void setBookRepository(BookRepository bookRepository) {
 	        this.bookRepository = bookRepository;
 	    }
