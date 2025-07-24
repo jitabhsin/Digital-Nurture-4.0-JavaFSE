0d24b27cbd1dbcd0c75dddfe2a9339cd356a1cdf
jitabhsin <jitabhsin@gmail.com>
2025-07-24 16:38:34 +0530

    3.ReactJS-HOL-scorecalculatorapp

---
+++ WEEK 6/3.ReactJS-HOL-scorecalculatorapp/code/scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import {CalculateScore} from '.././src/components/CalculatorScore';
+
+function App() {
+  return(
+    <div>
+      <CalculateScore Name={"ABHISHEK SINGH"}
+        School={"VSB COLLEGE"}
+        total={284}
+        goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
